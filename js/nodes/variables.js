e2d6155d25d2b97835d0443bf54cc070136aafb4
inventivetalent <mail@inventivetalent.org>
2019-01-03 22:42:53 +0100

    support for primitive variables

--- js/nodes/variables.js
+++ js/nodes/variables.js
@@ -5,13 +5,15 @@ function Set() {
     this.addInput("EXEC", "@EXEC", {shape: LiteGraph.ARROW_SHAPE, color_off: Colors.EXEC_OFF, color_on: Colors.EXEC_ON});
     this.addInput("myVariable", null);
     this.addProperty("name", "myVariable", "string");
+    this.addProperty("type", "string", "enum", {values: ["string", "byte", "char", "short", "int", "long", "float", "double", "any"]})
 }
 
 Set.prototype.onDrawBackground = function () {
     this.inputs[1].label = this.properties.name;
+    this.inputs[1].type = parseType(this.properties.type);
 };
 Set.prototype.getFields = function (output) {
-    return ["java.lang.Object " + this.properties.name];// TODO: support for primitive types
+    return [parseType(this.properties.type) + " " + this.properties.name];
 }
 Set.prototype.getMethodBody = function (input, output) {
     return this.properties.name + " = " + input[1] + ";";
@@ -22,17 +24,28 @@ function Get() {
     this.classType = "native";
     this.addOutput("myVariable", null);
     this.addProperty("name", "myVariable", "string");
+    this.addProperty("type", "string", "enum", {values: ["string", "byte", "char", "short", "int", "long", "float", "double", "any"]})
 }
 
 Get.prototype.onDrawBackground = function () {
     this.outputs[0].label = this.properties.name;
+    this.outputs[0].type = parseType(this.properties.type);
 };
 Get.prototype.getMethodBody = function (input, output) {
     return output[0] + " = " + this.properties.name + ";";
 };
 
+function parseType(type) {
+    if (!type || type === "any") {
+        type = "java.lang.Object"
+    } else if (type === "string") {
+        type = "java.lang.String";
+    }
+    return type;
+}
 
-module.exports=[
+
+module.exports = [
     Set,
     Get
 ];
