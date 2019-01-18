f01ab2587e9dbe95f098bdd47a8a2dba1786b070
inventivetalent <mail@inventivetalent.org>
2019-01-18 21:51:30 +0100

    add MakeArray

--- js/nodes/array.js
+++ js/nodes/array.js
@@ -1,6 +1,50 @@
 const Colors = require("../colors");
 const {shapeAndColorsForSlotType} = require("../util");
 
+
+function MakeArray() {
+    this.classType = "native";
+    this.addInput("EXEC", "@EXEC", shapeAndColorsForSlotType("@EXEC"));
+    this.addOutput("EXEC", "@EXEC", shapeAndColorsForSlotType("@EXEC"));
+    this.addInput("Element", "java.lang.Object");
+    this.addOutput("Array", "java.lang.Object[]", shapeAndColorsForSlotType("java.lang.Object"));
+
+    this.addProperty("type", "java.lang.Object", "string");
+
+    this.optional_inputs = [["Element", null, {}]];
+}
+
+MakeArray.title = "MakeArray";
+MakeArray.prototype.onPropertyChanged = function (p, v) {
+    if (p === "type") {
+        let shapeAndColor = shapeAndColorsForSlotType(this.properties.type);
+
+        this.outputs[1].type = this.properties.type + "[]";
+        this.outputs[1].shape = shapeAndColor.shape;
+        this.outputs[1].color_on = shapeAndColor.color_on;
+        this.outputs[1].color_off = shapeAndColor.color_off;
+        for (let i = 0; i < this.inputs.length; i++) {
+            if (i >= 1) {
+                this.inputs[i].type = this.properties.type;
+                this.inputs[i].shape = shapeAndColor.shape;
+                this.inputs[i].color_on = shapeAndColor.color_on;
+                this.inputs[i].color_off = shapeAndColor.color_off;
+            }
+        }
+    }
+};
+MakeArray.prototype.getFields = function (output) {
+    return [this.properties.type + "[] " + output[1]];
+};
+MakeArray.prototype.getMethodBody = function (input, output) {
+    let variableInputs = input.slice(1);
+    return output[1] + " = new "+this.properties.type+"[] {" + variableInputs.join(",") + "};";
+};
+MakeArray.prototype.getExecAfter = function (exec) {
+    return exec[0].join("\n");
+};
+
+
 function SetIndex() {
     this.classType = "native";
     this.addInput("EXEC", "@EXEC", shapeAndColorsForSlotType("@EXEC"));
@@ -44,7 +88,7 @@ function Length() {
     this.addInput("EXEC", "@EXEC", shapeAndColorsForSlotType("@EXEC"));
     this.addOutput("EXEC", "@EXEC", shapeAndColorsForSlotType("@EXEC"));
     this.addInput("Array", null);
-    this.addOutput("length", "int",shapeAndColorsForSlotType("int"));
+    this.addOutput("length", "int", shapeAndColorsForSlotType("int"));
 }
 
 Length.prototype.getFields = function (output) {
@@ -59,8 +103,8 @@ Length.prototype.getExecAfter = function (exec) {
 };
 
 
-
 module.exports = [
+    MakeArray,
     SetIndex,
     GetIndex,
     Length
